c23f377dcd219291de111379f6d46a8a84111d98
Robert <robert.arbon@gmail.com>
2017-06-27 14:48:01 +0100

    all the new training files

--- pickleSave.py
+++ pickleSave.py
@@ -2,4 +2,4 @@ import NNFlow
 import pickle
 
 silvia = NNFlow.MLPRegFlow(max_iter=1000)
-pickle.dump(silvia, open('optimisation/RSCM/attempt2/model.pickl','wb'))
+pickle.dump(silvia, open('optimisation/DPCCM/attempt1/model.pickl','wb'))
