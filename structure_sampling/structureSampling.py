f5db5f4e5ba0b59b32bf9f381024d0e2a4a993f9
SilviaAmAm <silvia.amabilino@bristol.ac.uk>
2017-07-19 12:11:30 +0100

    changes saved before rebase

--- structure_sampling/structureSampling.py
+++ structure_sampling/structureSampling.py
@@ -7,60 +7,58 @@ from sklearn import model_selection as modsel
 
 X, y, Q = ImportData.loadPd_q("/mnt/storage/home/sa16246/repositories/trainingNN/dataSets/PBE_B3LYP/pbe_b3lyp_partQ_rel.csv")
 
-# lst = [1000, 5000, 10000, 15000, 17000]
-lst = [10000]
+frac = 10000
 
-for frac in lst:
-	print "Started the procedure with " + str(frac) + " samples"
-	X_frac = X[:frac][:]
-	y_frac = y[:frac]
-	
-	# Generating coulomb matrix and trimmed coulomb matrix
-	CM = CoulombMatrix.CoulombMatrix(matrixX=X_frac)
-	X_tcm = CM.generateTrimmedCM()
-	
-	# Using the trimmed coulomb matrix to figure out the best splitting of training set and test set
-	train_idx = FFtraversal.fft_idx(X_tcm, int(0.8 * X_tcm.shape[0]))
+print "Started the procedure with " + str(frac) + " samples"
+X_frac = X[:frac][:]
+y_frac = y[:frac]
 
-	# Splitting the data set
-	X_train = []
-	y_train = []
-	
-	for item in train_idx:
-	    X_train.append(X_frac[item])
-	    y_train.append(y_frac[item])
-	
-	X_test = np.delete(X_frac, train_idx, axis=0)
-	y_test = np.delete(y_frac, train_idx, axis=0)
-	
-	# Generating the descriptor for the train set
-	des_train = CoulombMatrix.CoulombMatrix(matrixX=X_train)
-	X_prcm_train, y_train = des_train.generatePRCM(y_train,numRep=4)
-	
-	# Generating the descriptor for the test set
-	des_test = CoulombMatrix.CoulombMatrix(matrixX=X_test)
-	X_prcm_test, y_test = des_test.generatePRCM(y_test,numRep=4)
-	
-	# Defining the estimator
-	estimator = NNFlow.MLPRegFlow(max_iter=2000, batch_size=500, alpha=0.0005, learning_rate_init=0.0002, hidden_layer_sizes=(51,))
-	
-	# Set up the cross validation set, for doing 5 k-fold validation
-	cv_iter = modsel.KFold(n_splits=5)
-	
-	# Training the neural net
-	estimator.fit(X_prcm_train, y_train, X_prcm_test, y_test)
-	# estimator.plotLearningCurve()
-	# estimator.errorDistribution(X_prcm_test, y_test)
-	
-	print "For the training with " + str(frac) + " data points:"
-	r2, rmse, mae, lpo, lno = estimator.scoreFull(X_prcm_test, y_test)
-	print "On test set:"
-	print "R^2: " + str(r2)
-	print "rmse (kJ/mol): " + str(rmse)
-	print "mae (kJ/mol): " + str(mae)
-	print "The largest positive outlier (kJ/mol): " + str(lpo)
-	print "The largest negative outlier (kJ/mol): " + str(lno)
-	
-	
-	# Correlation plot
-	# estimator.correlationPlot(X_prcm_test, y_test, ylim=(-0.075,0.040), xlim=(-0.075,0.040))
+# Generating coulomb matrix and trimmed coulomb matrix
+CM = CoulombMatrix.CoulombMatrix(matrixX=X_frac)
+X_tcm = CM.generateTrimmedCM()
+
+# Using the trimmed coulomb matrix to figure out the best splitting of training set and test set
+train_idx = FFtraversal.fft_idx(X_tcm, int(0.8 * X_tcm.shape[0]))
+
+# Splitting the data set
+X_train = []
+y_train = []
+
+for item in train_idx:
+    X_train.append(X_frac[item])
+    y_train.append(y_frac[item])
+
+X_test = np.delete(X_frac, train_idx, axis=0)
+y_test = np.delete(y_frac, train_idx, axis=0)
+
+# Generating the descriptor for the train set
+des_train = CoulombMatrix.CoulombMatrix(matrixX=X_train)
+X_prcm_train, y_train = des_train.generatePRCM(y_train,numRep=4)
+
+# Generating the descriptor for the test set
+des_test = CoulombMatrix.CoulombMatrix(matrixX=X_test)
+X_prcm_test, y_test = des_test.generatePRCM(y_test,numRep=4)
+
+# Defining the estimator
+estimator = NNFlow.MLPRegFlow(max_iter=2000, batch_size=500, alpha=0.0005, learning_rate_init=0.0002, hidden_layer_sizes=(51,))
+
+# Set up the cross validation set, for doing 5 k-fold validation
+cv_iter = modsel.KFold(n_splits=5)
+
+# Training the neural net
+estimator.fit(X_prcm_train, y_train, X_prcm_test, y_test)
+# estimator.plotLearningCurve()
+# estimator.errorDistribution(X_prcm_test, y_test)
+
+print "For the training with " + str(frac) + " data points:"
+r2, rmse, mae, lpo, lno = estimator.scoreFull(X_prcm_test, y_test)
+print "On test set:"
+print "R^2: " + str(r2)
+print "rmse (kJ/mol): " + str(rmse)
+print "mae (kJ/mol): " + str(mae)
+print "The largest positive outlier (kJ/mol): " + str(lpo)
+print "The largest negative outlier (kJ/mol): " + str(lno)
+
+
+# Correlation plot
+# estimator.correlationPlot(X_prcm_test, y_test, ylim=(-0.075,0.040), xlim=(-0.075,0.040))
--- structure_sampling/submit.sh
+++ structure_sampling/submit.sh
@@ -2,6 +2,7 @@
 #
 #SBATCH -p gpu
 #SBATCH --nodes=1
+#SBATCH --mem=1500
 module load libs/tensorflow-1.2
 module unload Python/2.7.12-foss-2016b
 
