aa3b3ed602c9dad85845f1f5e2ef432c14e45782
SilviaAmAm <silvia.amabilino@bristol.ac.uk>
2017-10-11 13:25:45 +0100

    Committing before merging

--- pickleSave.py
+++ pickleSave.py
@@ -4,4 +4,4 @@ import NNFlow
 import pickle
 
 silvia = NNFlow.MLPRegFlow(max_iter=1000)
-pickle.dump(silvia, open('/mnt/storage/home/sa16246/repositories/trainingNN/optimisation/PBE_B3LYP/PRCM/not_scal/model.pickl','wb'))
+pickle.dump(silvia, open('/mnt/storage/home/sa16246/repositories/trainingNN/optimisation/PBE_B3LYP/hybrids/hyb3/model.pickl','wb'))
